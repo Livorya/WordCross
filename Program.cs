a1045fc4b19f97ba2e96628a7e4cde35deb8d513
plosgit <fadingcrap@gmail.com>
2025-01-08 11:04:38 +0100

    connectdatabase

--- Program.cs
+++ Program.cs
@@ -8,6 +8,5 @@ var app = builder.Build();
 app.UseDefaultFiles(); // Serving index.html as the default file
 app.UseStaticFiles(); // Serves other static files like CSS, JS, images, etc.
 
-//app.MapGet("/", () => "Hello World!");
 
 app.Run();
