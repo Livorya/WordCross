97a233f1471ca2c5e02aa8b3f1c2f94a652ee605
Malin W <malin.wiren@hotmail.com>
2025-01-10 15:22:45 +0100

    changed so we can close the modal by clicking it

--- wwwroot/app.js
+++ wwwroot/app.js
@@ -18,30 +18,24 @@ async function getAllWord(e) {
     $('#message').text('Change?');
 }
 
-var modal = document.getElementById("subject-modal");
+const modal = document.getElementById("subject-modal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("subjectTitle");
+const openModal = document.getElementById("subjectTitle");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const closeModal = document.getElementById("subject-modal-content");
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+openModal.onclick = function() {
     modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeModal.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 
 /*
 * #Id index: *
