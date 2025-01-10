6d6072649fd049fe9ed1b19b1e1ed90d804f0bc1
lakevalley <lakevalleymalmo@gmail.com>
2025-01-10 11:22:16 +0100

    added modal

--- wwwroot/app.js
+++ wwwroot/app.js
@@ -18,6 +18,31 @@ async function getAllWord(e) {
     $('#message').text('Change?');
 }
 
+var modal = document.getElementById("subject-modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("subjectTitle");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 /*
 * #Id index: *
 
